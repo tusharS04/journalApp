c7c36cd43be01e3487f766caa6bfb755b777e59b
Tushar Somvanshi <tusharsomvanshi0406@gmail.com>
2026-02-03 23:24:51 +0530

    Sending Email with Attachment

--- src/main/java/net/engineeringdigest/journalApp/service/EmailService.java
+++ src/main/java/net/engineeringdigest/journalApp/service/EmailService.java
@@ -2,9 +2,16 @@ package net.engineeringdigest.journalApp.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMailMessage;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @Service
 @Slf4j
@@ -13,6 +20,7 @@ public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    //----- simple text mail -----//
     public void sendEmail(String to, String subject, String body) {
         try {
             SimpleMailMessage mailMessage = new SimpleMailMessage();
@@ -23,6 +31,25 @@ public class EmailService {
         } catch (Exception e) {
             log.error("Error while sending mail{}", e.getMessage(), e);
             //throw new Exception("Error while sending mail" +e.getMessage(), e);
+        }
+    }
+
+    //----- mail with html and attachments-----//
+    public void sendMailWithAttachment(String to, String subject, String body,
+                                       String filePath, String fileName) throws MessagingException {
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            FileSystemResource file = new FileSystemResource(new File(filePath));
+            helper.addAttachment(fileName, file);
+
+            javaMailSender.send(message);
+        }  catch (Exception e) {
+            log.error("Error while sending mail{}", e.getMessage(), e);
+            throw new MessagingException("Error while sending mail" +e.getMessage(), e);
         }
     }
 }
--- src/test/java/net.egnineeringdigest.journalApp/service/EmailServiceTest.java
+++ src/test/java/net.egnineeringdigest.journalApp/service/EmailServiceTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.mail.MessagingException;
+
 @SpringBootTest
 public class EmailServiceTest {
 
@@ -17,6 +19,17 @@ public class EmailServiceTest {
                 "tusharsomvanshi0406@gmail.com",
                 "Test Mail",
                 "Hello ! How are you ?"
+        );
+    }
+
+    @Test
+    void sendMailWithAttachment() throws MessagingException {
+        emailService.sendMailWithAttachment(
+                "tusharsomvanshi0406@gmail.com",
+                "Email with attachment and HTML",
+                "<h3>Your Attachment</h3><p>Please find the attached document.</p>",
+                "C:\\Users\\Tushar\\Downloads\\invoice_PO10000880756-723.pdf",
+                "Attachment.pdf"
         );
     }
 }
